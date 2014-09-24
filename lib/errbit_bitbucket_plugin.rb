b095d2541413ec6cd924074ca6fb526d356b0e3d
Matthew McFarling <matthew.mcfarling@gmail.com>
2014-09-24 12:52:46 -0400

    hooking up the plugin to the issue tracker registry

--- lib/errbit_bitbucket_plugin.rb
+++ lib/errbit_bitbucket_plugin.rb
@@ -1,5 +1,12 @@
-require "errbit_bitbucket_plugin/version"
+require 'errbit_bitbucket_plugin/version'
+require 'errbit_bitbucket_plugin/error'
+require 'errbit_bitbucket_plugin/issue_tracker'
+require 'errbit_bitbucket_plugin/rails'
 
 module ErrbitBitbucketPlugin
-  # Your code goes here...
+  def self.root
+    File.expand_path '../..', __FILE__
+  end
 end
+
+ErrbitPlugin::Registry.add_issue_tracker(ErrbitBitbucketPlugin::IssueTracker)
